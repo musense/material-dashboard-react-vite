60d79c2210cf556685298bd0df091f1732a827f7
musense <musense.ad@gmail.com>
2023-09-20 10:05:16 +0800

    修改 LOGO 圖檔位置並移動圖檔位置

--- src/components/Sidebar/LogoImage.jsx
+++ src/components/Sidebar/LogoImage.jsx
@@ -4,7 +4,7 @@ const LogoImage = ({ className }) => {
     const logoDir = import.meta.env.VITE_LOGO_DIR;
     const description = import.meta.env.VITE_LOGO_TEXT;
     return <img
-        src={`/src/assets/img/${logoDir}`}
+        src={`/${logoDir}`}
         alt={description}
         className={className}
     />;
